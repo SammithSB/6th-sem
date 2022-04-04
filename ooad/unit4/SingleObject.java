849a3cc0d6d8b690b05529a277e8caf591f73789
SammithSB <sammithsb@gmail.com>
2022-04-04 14:02:15 +0530

    singleton pattern

---
+++ ooad/unit4/SingleObject.java
@@ -0,0 +1,21 @@
+package unit4;
+
+public class SingleObject {
+    // create a single onject
+    private static SingleObject instance = new SingleObject();
+
+    // make construtor private wo this class cannot be instantiated
+    private SingleObject() {
+    }
+
+    // get the only object available
+    public static SingleObject getInstance() {
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello World");
+    }
+
+
+}
---
+++ ooad/unit4/SingletonPatternDemo.java
@@ -0,0 +1,11 @@
+package unit4;
+
+public class SingletonPatternDemo {
+    public static void main(String[] args) {
+        //illegal construct
+        //SingleObject object = new SingleObject();
+        // get the only object available
+        SingleObject singleton = SingleObject.getInstance();
+        singleton.showMessage();
+    }
+}
